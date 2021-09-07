624704e9d3245ee7e2cadd9e5cbde091367f7299
Marko Goronjic <goronjicmarko24@gmail.com>
2021-09-07 15:34:42 +0200

    Refs vs Reactice

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,37 @@
 <template>
   <div class="home">
-    home
-    <p ref="para">My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">Click Me</button>
-    <button @click="age++">Add 1 to Age</button>
-    <input type="text" v-model="name">
+    <h1>Home</h1>
+    <h2>Refs</h2>
+    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+    <button @click="updateNinjaOne">Update Ninja one</button>
+    <h2>Reactive</h2>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }} - {{ nameTwo }}</p>
+    <button @click="updateNinjaTwo">Update Ninja Two</button>
   </div>
 </template>
 
 <script>
-import { ref } from '@vue/reactivity';
+import { ref, reactive } from '@vue/reactivity';
 
 
 export default {
   name: 'Home',
   setup() {
 
-    // const para = ref(null);
+   const ninjaOne = ref({ name: 'mario', age: 30});
+   const ninjaTwo = reactive({ name: 'luigi', age: 32});
+   
+   const nameOne = ref('mario');
+   const nameTwo = reactive('luigi');
 
-    const name = ref('mario');
-    const age = ref(20);
+   const updateNinjaOne = () => {
+     ninjaOne.value.age = 40;
+   }
+   const updateNinjaTwo = () => {
+     ninjaTwo.age = 40;
+   }
 
-    const handleClick = () => {
-      name.value = 'yoshi';
-      age.value = 30
-    }
-
-    return { name, age, handleClick };
+    return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo, nameTwo };
   }
 }
 </script>
