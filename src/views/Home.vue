3a09e9730ee7382f06e0183331746a2c52684eba
Marko Goronjic <goronjicmarko24@gmail.com>
2021-09-08 09:39:48 +0200

    Lifecycle hooks in setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList :posts="posts" />
+    <PostList v-if="showPosts" :posts="posts" />
+    <button @click="showPosts = !showPosts">Toggle Posts</button>
+    <button @click="posts.pop()">Delete a post</button>
   </div>
 </template>
 
 <script>
-import PostList from '../components/PostList.vue'
+import PostList from '../components/PostList.vue';
 import { ref } from '@vue/reactivity';
 
 
@@ -15,12 +17,14 @@ export default {
   components: { PostList },
   setup() {
     const posts = ref([ 
-        {title: 'Welcome to the blog', body: 'Lorem ipsum', id: 1},
-        {title: 'Top 5 CSS tipts', body: 'Lorem ipsum', id: 2},
+        {title: 'Welcome to the blog', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in imperdiet nisi. Vestibulum eu dui dictum, ornare est non, dignissim dui. Donec at ex nulla. Aenean sem tortor, bibendum ac accumsan vel, elementum vel turpis. Nam auctor egestas pulvinar. Aenean placerat finibus finibus. Praesent dictum velit at purus aliquam, sit amet mattis est molestie. Duis a nulla et mauris luctus feugiat nec cursus odio. Nam elementum vitae est eu porttitor. Nulla vestibulum gravida magna eu ultricies. Nunc posuere tincidunt pellentesque. Sed id condimentum nisl, ultricies viverra orci. Fusce sit amet massa ut nibh feugiat elementum ac id est."', id: 1},
+        {title: 'Top 5 CSS tipts', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in imperdiet nisi. Vestibulum eu dui dictum, ornare est non, dignissim dui. Donec at ex nulla. Aenean sem tortor, bibendum ac accumsan vel, elementum vel turpis. Nam auctor egestas pulvinar. Aenean placerat finibus finibus. Praesent dictum velit at purus aliquam, sit amet mattis est molestie. Duis a nulla et mauris luctus feugiat nec cursus odio. Nam elementum vitae est eu porttitor. Nulla vestibulum gravida magna eu ultricies. Nunc posuere tincidunt pellentesque. Sed id condimentum nisl, ultricies viverra orci. Fusce sit amet massa ut nibh feugiat elementum ac id est."', id: 2},
      ])
 
+     const showPosts = ref(true)
+
     
-    return { posts };
+    return { posts, showPosts };
   }
 }
 </script>
